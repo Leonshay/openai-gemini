40945b981651c73a32ff6cfcf21313a03cb58a56
00103936 <00103936@lesso.com>
2025-04-09 17:00:08 +0800

    refactor(worker): 优化 thinking模型响应处理
    - 移除了多余的 console.log 语句
    - 合并了日志输出，减少冗余信息
    -保留了必要的日志输出，便于调试

--- src/worker.mjs
+++ src/worker.mjs
@@ -989,8 +989,7 @@ function transformResponseStream(data, stop, first) {
 
 function transformThinkingResponseStream(data, stop, first) {
   const item = transformThinkingCandidatesDelta(data.candidates[0]);
-  console.log("item.content.parts: ",item.content.parts)
-  console.log("item.content.parts[0].text: ",item.content.parts[0].text)
+  console.log("item.content.parts[0]: ", item.content.parts?.[0], "item.content.parts[0].text: ",item.content.parts[0].text);
   if (item?.content?.parts?.[0]?.text) {thinkingChunks.push(item.content.parts[0].text);}
   if (stop) {
     item.delta = {};
